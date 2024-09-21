d82202d91a84b3ab314bddb91d562c85adb7a292
doseung <didehtmd@gmail.com>
2024-09-21 14:31:37 +0900

    로고 변경

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,8 @@
 "use client";
 export default function Header() {
   return (
-    <header className="w-full bg-green-700 py-3 px-4">
-      <img src="/wadiz.png" alt="wadiz Logo" className="h-7" />
+    <header className="w-full bg-gray-700 py-3 px-4">
+      <img src="/noteLogo.png" alt="doseung-logo" className="h-7" />
     </header>
   );
 }
